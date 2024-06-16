3c0d568f415ce133d7543262df17eaea9746997e
MDilshodbek <mdbek55@gmail.com>
2024-06-16 23:07:48 +0900

    updated

--- greenShop2/src/main.jsx
+++ greenShop2/src/main.jsx
@@ -1,6 +1,25 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import createStore from "react-auth-kit/createStore";
+import AuthProvider from "react-auth-kit";
+import { Provider } from "react-redux";
+import store from "./redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const authStore = createStore({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === "https:",
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider store={authStore}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </AuthProvider>
+);
