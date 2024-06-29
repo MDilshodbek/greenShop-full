00223825d7954dcd2e362f94e4f4c5ad5be09571
MDilshodbek <mdbek55@gmail.com>
2024-06-29 21:28:52 +0900

    updated with auth

--- greenShop/src/components/navbar/modals/auth/login/index.jsx
+++ greenShop/src/components/navbar/modals/auth/login/index.jsx
@@ -1,17 +1,57 @@
 import {
   FacebookFilled,
   GoogleOutlined,
+  LoadingOutlined,
   ScanOutlined,
 } from "@ant-design/icons";
-import { Divider, Form, Input } from "antd";
+import { Divider, Form, Input, notification } from "antd";
+import { useState } from "react";
+import useAxios from "../../../../../hooks/axios";
+import useSignIn from "react-auth-kit/hooks/useSignIn";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const axios = useAxios();
+  const signIn = useSignIn();
+
+  const onFinish = async (e) => {
+    setLoading(true);
+    try {
+      const { data } = await axios({
+        method: "POST",
+        url: "/user/sign-in",
+        data: e,
+      });
+      const { token, user } = data.data;
+
+      signIn({
+        auth: {
+          token,
+          type: "Bearer",
+        },
+        userState: user,
+      });
+
+      notification.success({ message: "Logged in", description: "You have logged in successfully" });
+
+      console.log(data);
+    } catch (error) {
+      notification.error({
+        message: "Oops! Something went wrong!",
+        description: error?.response?.data?.extraMessage,
+      });
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="w-[80%] m-auto">
       <h3 className="text-sm mt-8 font-notmal">
         Enter your username and password to login
       </h3>
       <Form
+        onFinish={onFinish}
         className="mt-4"
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 32 }}
@@ -21,10 +61,10 @@ const Login = () => {
         layout="vertical"
       >
         <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
         >
-          <Input placeholder="input your username..." />
+          <Input placeholder="input your email..." />
         </Form.Item>
 
         <Form.Item
@@ -34,18 +74,12 @@ const Login = () => {
           <Input.Password placeholder="input your password..." />
         </Form.Item>
 
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        ></Form.Item>
-
         <h3 className=" font-normal cursor-pointer w-fit ml-auto text-[#46A358]">
           Forgot Password?
         </h3>
 
         <button className="flex rounded-md bg-[#46A358] gap-1 text-base items-center justify-center text-white w-full h-[45px] my-[27px]">
-          Login
+          {loading ? <LoadingOutlined /> : "Login"}
         </button>
       </Form>
 
